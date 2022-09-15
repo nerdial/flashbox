114032f4effd8edd1e292f9f3ad8bd7c19cad89a
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-09-15 22:46:56 +0430

    Created all the migrations at once.

---
+++ database/migrations/2022_09_14_190506_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('address')->nullable();
+            $table->decimal('lat');
+            $table->decimal('lng');
+            $table->foreignIdFor(\App\Models\User::class,'seller_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+};
