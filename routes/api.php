e8ec5b95ddb5fedb45d4089f1bbcf2826e38cc66
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-09-15 23:40:13 +0430

    Add api routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\SellerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+
+    Route::prefix('app')->group(function () {
+
+        Route::get('user', [UserController::class, 'currentUser']);
+
+
+        // seller routes
+        Route::prefix('seller')->group(function () {
+            Route::get('products',
+                [SellerController::class,
+                    'getAllProducts']);
+            Route::post('products/create', [SellerController::class,
+                'storeProduct']);
+        });
+
+
+        // admin routes
+        Route::prefix('admin/sellers')->group(function () {
+            Route::get('/', [AdminController::class, 'sellerList']);
+            Route::post('create', [AdminController::class,
+                'storeNewSeller']);
+        });
+
+
+        // customer routes
+        Route::prefix('customer')->group(function () {
+            Route::get('products', [CustomerController::class, 'getNearbyProducts']);
+            Route::post('products/purchase/{product}', [CustomerController::class,
+                'purchaseAnItem']);
+        });
+
+    });
 });
