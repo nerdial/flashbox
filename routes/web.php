1a3caa374a36d7efd3aeb2900e46455545a2e5c8
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-09-15 23:21:54 +0430

    Add route for login request

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+
+
+Route::post('login', [AuthController::class, 'login'])->name('login');
